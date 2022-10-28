59a95ffa412b724b43bd86603cf192ffe0edeb72
KacperGrabowy <kacper.grabowy01@gmail.com>
2022-10-28 17:43:43 +0100

    Added functionality to Login page. Toast if creds are incorrect. If correct go to register page.

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,26 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     // Variables declared
     MaterialButton loginbtn;
     TextView username, password, registerHere;
     ImageView ckpm;
-    // get rid later
-    int i;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+
 
         // Initializing variables
         username =(TextView) findViewById(R.id.username);
@@ -29,19 +31,28 @@ public class MainActivity extends AppCompatActivity {
         loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
         ckpm = (ImageView) findViewById(R.id.ckpm);
 
-        // Press LOGIN button will bring user to new page
+
+
+        // Pressing LOGIN button will bring user to new page
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Checking username and password
                 // Will check for user in database later
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    // ignore for now just adding so can test
-                    i++;
-                }else
-                    // ignore for now just adding so can test
-                    i++;
+                    // Switching to register page by calling open activity method
+                    openRegisterActivity();
+
+                } else
+                    // If login details provided are incorrect display message to user
+                    Toast.makeText(LoginActivity.this,"Credentials Entered Incorrect!!",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    // this class will open the new register activity which holds the register form
+    public void openRegisterActivity() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
