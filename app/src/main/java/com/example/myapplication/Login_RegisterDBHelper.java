2e6fa46d8c1414bf812a57e8b9363bd2f825d6fb
Cormac.Kelly <cormacgerald@gmail.com>
2022-11-07 02:23:16 +0000

    "Login and Register - SQL Lite Functionality Working - Minor error handling may need to be modified"

--- app/src/main/java/com/example/myapplication/Login_RegisterDBHelper.java
+++ app/src/main/java/com/example/myapplication/Login_RegisterDBHelper.java
@@ -5,13 +5,13 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
 public class Login_RegisterDBHelper extends SQLiteOpenHelper {
 
-    public static final String DBName = "Login.db";
-
+    public static final String DBName = "login2.db";
 
     public Login_RegisterDBHelper(Context context) {
         super(context, DBName, null, 1);
@@ -19,21 +19,25 @@ public class Login_RegisterDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table users(username TEXT primary key, password TEXT)");
+        sqLiteDatabase.execSQL("create table users(username TEXT primary key, password TEXT, email TEXT, membership_type TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("Drop table if exists users");
+        onCreate(sqLiteDatabase);
     }
 
 
-    public Boolean inputData (String username, String password) {
+     public Boolean inputData (String username, String password, String mail, String membership) {
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues value = new ContentValues();
 
         value.put("username", username);
         value.put("password", password);
+        value.put("email",mail);
+         value.put("membership_type",membership);
+
 
         long result = sqLiteDatabase.insert("users",null, value);
 
@@ -43,15 +47,6 @@ public class Login_RegisterDBHelper extends SQLiteOpenHelper {
             return true;
     }
 
-    public Boolean checkUserName(String username) {
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        Cursor cursor = sqLiteDatabase.rawQuery("select * from users where username=?", new String[] {username});
-        if (cursor.getCount() > 0){
-            return true;
-        }
-        else
-            return false;
-    }
 
     public Boolean checkUsernamePassword(String username, String password){
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
